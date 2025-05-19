3e5223c8ce186c7ae09500e2860521319b5147a9
Natalia <natnlg8@gmail.com>
2025-05-19 13:13:30 -0700

    index and calculate

---
+++ index.php
@@ -0,0 +1,6 @@
+<form action="calculate.py" method="get">
+    A: <input type="number" name="a"><br>
+    B: <input type="number" name="b"><br>
+    C: <input type="number" name="c"><br>
+    <input type="submit" value="Calculate">
+</form>
